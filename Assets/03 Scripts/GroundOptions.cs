ee5519a871bb3a12778f4ae8b5d5a6a478900127
Ferran Bové <ferranboverexach@gmail.com>
2017-03-03 11:26:47 +0100

    Ground + Room (initial)
    Ground amb textures no definitives i Room començat

---
+++ Assets/03 Scripts/GroundOptions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundOptions : MonoBehaviour {
+    public enum groundTypes { castell, casa, gespa, cami};
+    public groundTypes Type= groundTypes.gespa;
+    SpriteRenderer sRenderer;
+    public Sprite[] spriteArray;
+	// Use this for initialization
+	void Start () {
+        sRenderer = this.GetComponent<SpriteRenderer>();
+        sRenderer.sprite = spriteArray[(int)Type];
+	}
+	
+}
+
---
+++ Assets/03 Scripts/RoomControler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomControler : MonoBehaviour {
+    public GameObject GroundPrefab;
+    public int x;
+    public int y;
+    public int midax = 50;
+
+	// Use this for initialization
+	void Start () {
+        GameObject actual;
+	for(int i=0; i<x; i++)
+        {
+            actual=Instantiate(GroundPrefab);
+            actual.transform.position = new Vector3(i*midax,0,0); //afegir Y
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
