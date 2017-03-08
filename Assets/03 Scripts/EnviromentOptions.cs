bd50f3fc61ddec316545b8f53d433562827eaf3a
Miquel Barnada <Milkbarna@gmail.com>
2017-03-08 13:58:02 +0100

    Room size generator
    + manual created initial

--- Assets/03 Scripts/EnviromentOptions.cs
+++ Assets/03 Scripts/EnviromentOptions.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GroundOptions : MonoBehaviour {
-    public enum groundTypes { castell, casa, gespa, cami};
-    public groundTypes Type= groundTypes.gespa;
+public class EnviromentOptions : MonoBehaviour {
+
+    public enum enviromentTypes { castell_g, casa_g, gespa_g, cami_g, castell_w, casa_w, forest_w, cabe_w };
+    public enviromentTypes Type = enviromentTypes.gespa_g;
     SpriteRenderer sRenderer;
     public Sprite[] spriteArray;
+
 	// Use this for initialization
 	void Start () {
         sRenderer = this.GetComponent<SpriteRenderer>();
--- Assets/03 Scripts/RoomControler.cs
+++ Assets/03 Scripts/RoomControler.cs
@@ -3,23 +3,55 @@ using System.Collections;
 
 public class RoomControler : MonoBehaviour {
     public GameObject GroundPrefab;
+    public GameObject WallPrefab;
     public int x;
     public int y;
-    public int midax = 50;
+    public float midaX = 3;
+    public float midaY = 4;
 
-	// Use this for initialization
-	void Start () {
-        GameObject actual;
-	for(int i=0; i<x; i++)
-        {
-            actual=Instantiate(GroundPrefab);
-            actual.transform.position = new Vector3(i*midax,0,0); //afegir Y
+    // Use this for initialization
+    void Start () {
 
-        }
-	}
+        buildGround();
+        buildWalls();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void buildGround() {
+        GameObject actual;
+        for (int i = 0; i < x; i++) //generates floor with x-y size
+        {
+            for (int j = 0; j < y; j++)
+            {
+                actual = Instantiate(GroundPrefab);
+                actual.transform.position = new Vector3(i * midaX, j * midaY, 0);
+            }
+        }
+    }
+
+    void buildWalls() {
+        GameObject actual;
+        //Top & Down
+        for (int i = 0; i < x; i++)
+        {
+            actual = Instantiate(WallPrefab);
+            actual.transform.position = new Vector3(i * midaX, (y)*midaY, 0);
+
+            actual = Instantiate(WallPrefab);
+            actual.transform.position = new Vector3(i * midaX, (-1) * midaY, 0);
+        }
+        //Left & Right
+        for (int j = 0; j < y; j++)
+        {
+            actual = Instantiate(WallPrefab);
+            actual.transform.position = new Vector3(x * midaX, j * midaY, 0);
+
+            actual = Instantiate(WallPrefab);
+            actual.transform.position = new Vector3((-1) * midaX, j * midaY, 0);
+        }
+    }
 }
